621eb21ee65b7e723d712a31dccadf650d943ea3
Ilnur Baltaev <61975462+Winterborn-IT@users.noreply.github.com>
2022-02-12 15:43:07 +0400

    hw4 commit 1.0

--- src/main/java/level2/lesson4/Controller.java
+++ src/main/java/level2/lesson4/Controller.java
@@ -1,5 +1,18 @@
 package level2.lesson4;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField textField;
+    public TextArea textArea;
+
+    public void sendMessage(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+
+    }
 
 }
